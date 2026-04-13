b939799f146c6a549b936dc8f29d7a1f44fc7d73
Tobiasz Laskowski <tobil4sk@outlook.com>
2026-04-13 13:33:50 +0100

    Add 404 page

--- Main.hx
+++ Main.hx
@@ -17,6 +17,10 @@ class Home extends Page {
 	public function new() super(hhp.Hhp.render("home.htm"));
 }
 
+class Error404 extends Page {
+	public function new() super(hhp.Hhp.render("404.htm"));
+}
+
 function saveContent(fileName:String, content:Template) {
 	File.saveContent(Path.join([OUTPUT, fileName]), content.execute());
 }
@@ -25,6 +29,7 @@ function main() {
 	sys.FileSystem.createDirectory(OUTPUT);
 
 	saveContent("index.htm", new Home());
+	saveContent("404.htm", new Error404());
 
 	File.copy("templates/style.css", Path.join([OUTPUT, "style.css"]));
 }
